545f2de562806e34ead07417edf5a3ad90cbf7fe
Vignesh Kasturi <vigneshkasthuri2009@gmail.com>
2025-07-03 18:05:30 +0530

    optimized code

--- routes/circuitRoutes.js
+++ routes/circuitRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { getStatusSummary, processPayment, getCircuitBreakerSummary, getMetrics } = require('../controllers/circuitController');
-const { validate } = require('../middleware/validation');
 
 router.get('/summary', getStatusSummary);
 router.get('/llm-summary', getCircuitBreakerSummary);
